fd19f51f9801f7c36dc9fb2b1a4e88a51f9841c2
flamecaptain <jkempe04@gmail.com>
2019-11-18 16:06:49 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.*;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.stream.*;
+
+@TeleOp
+public class opmode extends LinearOpMode {
+    private DcMotor   frontLeft, frontRight, backLeft, backRight;
+    public void runOpMode()
+    {
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        while(opModeIsActive())
+        {
+            frontLeft.setPower(gamepad1.left_stick_y);
+            backLeft.setPower(gamepad1.left_stick_y);
+            frontRight.setPower(gamepad1.right_stick_y);
+            backRight.setPower(gamepad1.right_stick_y);
+        }
+    }
+}
