3dbd15e67db051d4ca4d74205b8f5624e504982c
Nathan Goldberg <naftalimaer2003@gmail.com>
2019-12-09 08:05:31 -0500

    Update AutoOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -4,11 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 @Autonomous
 
 public class AutoOpMode extends LinearOpMode {
     private DcMotor frontLeft, backLeft, frontRight, backRight, leftIntake, rightIntake, clawRaise;
-    private Servo foundation;
+    private Servo foundation, clawGrab;
     private ElapsedTime runTime = new ElapsedTime();
 
     private void setPower(double bl, double br, double fl, double fr)
@@ -23,7 +26,7 @@ public class AutoOpMode extends LinearOpMode {
         leftIntake.setPower(power);
         rightIntake.setPower(power);
     }
-    public void runOpMode(){
+    public void runOpMode() {
         backRight = hardwareMap.dcMotor.get("backRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         frontRight = hardwareMap.dcMotor.get("frontRight");
@@ -32,17 +35,35 @@ public class AutoOpMode extends LinearOpMode {
         rightIntake = hardwareMap.dcMotor.get("rightIntake");
         clawRaise = hardwareMap.dcMotor.get("clawRaise");
         foundation = hardwareMap.servo.get("foundation");
+        clawGrab = hardwareMap.servo.get("clawGrab");
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         leftIntake.setDirection(DcMotorSimple.Direction.REVERSE);
-        double f = .5;
-        double b = -.5;
-        double s = 0;
         runTime.reset();
         start();
         while(opModeIsActive())
         {
+            if(runTime.seconds() < 1)
+            {
+                clawGrab.setPosition(0);
+            }
+            else if(runTime.seconds() < 2)
+            {
+                clawGrab.setPosition(0.5);
+            }
+            else if(runTime.seconds() < 3)
+            {
+                clawGrab.setPosition(1);
+            }
+            else if(runTime.seconds() < 4)
+            {
+                clawGrab.setDirection(Servo.Direction.REVERSE);
+                clawGrab.setPosition(0.5);
+            }
+            else if(runTime.seconds() < 5)
+            {
 
+            }
         }
     }
 }
