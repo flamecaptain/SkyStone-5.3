40b64d8239b168c32e7f54847c20bb1aa6aa213b
Nathan Goldberg <naftalimaer2003@gmail.com>
2019-12-10 21:27:29 -0500

    Update TestIntake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIntake.java
@@ -7,32 +7,18 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
 public class TestIntake extends LinearOpMode {
-    private DcMotor leftIntake, rightIntake, clawRaise;
-    private Servo clawGrab, clawPancake;
-    private ElapsedTime runTime;
+    private DcMotor leftIntake, rightIntake;
     @Override
     public void runOpMode() throws InterruptedException {
         leftIntake = hardwareMap.dcMotor.get("leftIntake");
         rightIntake = hardwareMap.dcMotor.get("rightIntake");
-        clawRaise = hardwareMap.dcMotor.get("clawRaise");
-        clawGrab = hardwareMap.servo.get("clawGrab");
-        clawPancake = hardwareMap.servo.get("clawPancake");
         leftIntake.setDirection(DcMotorSimple.Direction.REVERSE);
         start();
-        runTime.reset();
         leftIntake.setPower(1);
         rightIntake.setPower(1);
         while(opModeIsActive())
         {
-            switch((int)runTime.seconds())
-            {
-                case 2: leftIntake.setPower(0); rightIntake.setPower(0); break;
-                case 3: clawGrab.setPosition(0.1); break;
-                case 5: clawRaise.setPower(0.3); break;
-                case 7: clawRaise.setPower(0); break;
-                case 8: clawPancake.setPosition(0.3);
-                case 10: clawGrab.setDirection(Servo.Direction.REVERSE); clawGrab.setPosition(0); break;
-            }
+
         }
     }
 }
