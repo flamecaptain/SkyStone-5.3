7e5be37a31f37e525c918cd90ef5bd30ecd2722c
Nathan Goldberg <naftalimaer2003@gmail.com>
2019-12-10 21:27:31 -0500

    Update TestSlide.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSlide.java
@@ -2,21 +2,36 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
 public class TestSlide extends LinearOpMode {
-    private DcMotor leftIntake, rightIntake;
+    private DcMotor leftIntake, rightIntake, clawRaise;
+    private Servo clawGrab, clawPancake;
+    private ElapsedTime runTime;
     @Override
     public void runOpMode() throws InterruptedException {
         leftIntake = hardwareMap.dcMotor.get("leftIntake");
         rightIntake = hardwareMap.dcMotor.get("rightIntake");
+        clawRaise = hardwareMap.dcMotor.get("clawRaise");
+        clawGrab = hardwareMap.servo.get("clawGrab");
+        clawPancake = hardwareMap.servo.get("clawPancake");
+        runTime = new ElapsedTime();
         leftIntake.setDirection(DcMotorSimple.Direction.REVERSE);
         start();
+        runTime.reset();
         while(opModeIsActive())
         {
-
+            switch((int)runTime.seconds())
+            {
+                case 2: leftIntake.setPower(0); rightIntake.setPower(0); break;
+                case 3: clawGrab.setPosition(0.1); break;
+                case 5: clawRaise.setPower(0.3); break;
+                case 7: clawRaise.setPower(0); break;
+                case 8: clawPancake.setPosition(0.3);
+                case 10: clawGrab.setDirection(Servo.Direction.REVERSE); clawGrab.setPosition(0); break;
+            }
         }
     }
 }
